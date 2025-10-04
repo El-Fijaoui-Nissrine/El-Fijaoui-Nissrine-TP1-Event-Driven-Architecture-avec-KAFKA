c3c4011d358b224d84dd46aea28a74d929273b0a
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-10-04 21:56:06 +0100

    AddHandlers

---
+++ kafka-spring-cloud-stream/src/main/java/com/example/kafka_spring_cloud_stream/handlers/PageEventHandler.java
@@ -0,0 +1,37 @@
+package com.example.kafka_spring_cloud_stream.handlers;
+
+import com.example.kafka_spring_cloud_stream.events.PageEvent;
+import org.springframework.stereotype.Component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Component
+public class PageEventHandler {
+    @Bean
+    public Consumer<Message<PageEvent>> pageEventConsumer(){
+        return (input)->{
+            System.out.println("*******************");
+            System.out.println(input.getPayload());
+            System.out.println("*****************");
+        };
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+new Random().nextInt(10000)
+            );
+        };
+    }
+
+}
